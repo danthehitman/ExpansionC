e2845168eab8a5953569b3c59e1fcc19f3b61e49
Dan Frank <danthehitman@gmail.com>
2017-08-25 22:48:51 -0600

    Adjusting buildcmd.bat to clean up the output a little.

--- handmade/x64buildcmd.bat
+++ handmade/x64buildcmd.bat
@@ -3,4 +3,4 @@ REM cmd.exe /k "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
 
-cmd.exe /k "build.bat"
+call "build.bat"
