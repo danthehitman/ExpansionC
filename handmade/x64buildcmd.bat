f3ee24a5003fb721673fe2389df0a03c59bd0640
Dan Frank <dan.frank@hpe.com>
2019-02-28 10:05:16 -0700

    WIP

--- handmade/x64buildcmd.bat
+++ handmade/x64buildcmd.bat
@@ -1,6 +1,6 @@
 @echo off
 REM cmd.exe /k "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 call "build.bat"
